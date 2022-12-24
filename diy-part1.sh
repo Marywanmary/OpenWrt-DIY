e7b2d7d5371799b46b0a85089b9ffb1c95566f3f
Marywanmary <70870062+Marywanmary@users.noreply.github.com>
2022-12-24 22:13:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,10 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'Add a feed source添加软件源'
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default #原始记录
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default #原始记录
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default #添加kenzok8/small-package
+#echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default #添加liuran001/openwrt-packages
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default #添加kiddin9/openwrt-packages
