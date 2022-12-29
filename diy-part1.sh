99dff072683d7e666f0ca089014ead4a7a94acb7
Marywanmary <70870062+Marywanmary@users.noreply.github.com>
2022-12-29 20:15:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,11 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+echo '添加helloworld/vssr软件源'
+rm -rf package/lean/helloworld
+rm -rf package/helloworld
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 
 echo 'Add a feed source添加软件源'
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default #原始记录
@@ -20,3 +25,9 @@ echo 'Add a feed source添加软件源'
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default #添加kenzok8/small-package
 #echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default #添加liuran001/openwrt-packages
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default #添加kiddin9/openwrt-packages
+
+#1.'asus_dhd24' 2.'ac88u_20' 3.'69027'
+#firmware='ac88u_20'
+#echo '替换无线驱动'
+#wget -nv https://github.com/yangxu52/Phicomm-k3-Wireless-Firmware/raw/master/brcmfmac4366c-pcie.bin.${firmware} -O package/lean/k3-brcmfmac4366c-firmware/files/lib/firmware/brcm/brcmfmac4366c-pcie.bin
+#echo '=========Replace k3 wireless firmware OK!========='
